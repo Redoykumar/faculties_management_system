134aa112627dca951420da94b37175fae3e583ae
Redoykumar <redoykumar0@gmail.com>
2022-11-16 04:05:06 +0600

    update :sideber and topbar

---
+++ src/components/sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-navigation-drawer v-if="drawer">
+        <v-img
+            src="https://media.istockphoto.com/id/1318957901/vector/the-green-background-with-a-white-gradient-is-perfect-for-those-of-you-who-are-looking-for-a.jpg?s=612x612&w=0&k=20&c=EXWdCg9600DkVRDKd7YkqOlXFZpC8EiFtSwUlJ8xaqE=">
+            <div class="text-center mt-7">
+                <v-avatar class="mb-5 " color="grey-darken-2" size="64">
+                    <v-img aspect-ratio="30" src="../assets/redoy.jpg">
+
+                    </v-img>
+                </v-avatar>
+                <h4 class="text-white bg-dark">redoykumar0@gmail.com</h4>
+            </div>
+
+        </v-img>
+        <!-- <v-sheet color="grey-lighten-4" class="pa-4">
+            <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+
+            <div>redoykumar0@gmail.com</div>
+        </v-sheet> -->
+
+        <v-divider></v-divider>
+
+        <v-list>
+            <v-list-item v-for="[icon, text, link] in links" :key="icon" link>
+                <template v-slot:prepend>
+                    <v-icon>{{ icon }}</v-icon>
+                </template>
+                <router-link :to="link" style="text-decoration: none; color: inherit;">
+                    <v-list-item-title>{{ text }}</v-list-item-title>
+                </router-link>
+
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: 'side-bar',
+    props: ['drawer'],
+    data: () => ({
+        get: function () {
+            return this.drawer;
+        },
+        links: [
+            ['mdi-monitor-dashboard', 'Dashboard', '/Dashboard'],
+            ['mdi-account-details-outline', 'Professors', 'Professors'],
+            ['mdi-account-group', 'Students', 'Students'],
+            ['mdi-bookshelf', 'Courses', 'Courses'],
+            ['mdi-book-open-page-variant-outline', 'Subjects', 'Subjects'],
+            ['mdi-information-outline', 'System Log', 'SystemLog'],
+        ],
+    }),
+}
+</script>
---
+++ src/components/topbar.vue
@@ -0,0 +1,130 @@
+<template>
+    <v-app-bar color="white" elevate-on-scroll scroll-target="#scrolling-techniques-7">
+        <v-app-bar-nav-icon @click="$emit('openDrawer')"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Faculties Management System</v-toolbar-title>
+        <!-- <v-col lg="5" cols="8">
+            <v-form class="mt-5">
+                <v-text-field rounded outlined dence placeholder="Search Here" append-icon="mdi-magnify"></v-text-field>
+            </v-form>
+        </v-col> -->
+
+        <v-spacer></v-spacer>
+
+        <!-- <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn> -->
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn v-bind="props">
+                    <div class="text-center">
+                        <v-badge :value="hover" color="deep-purple accent-4" :content="items.length" left
+                            transition="slide-x-transition">
+                            <v-hover v-model="hover">
+                                <v-icon color="grey lighten-1" large>
+                                    mdi-bell
+                                </v-icon>
+                            </v-hover>
+                        </v-badge>
+                    </div>
+                </v-btn>
+            </template>
+            <v-list :items="items" item-props lines="three" width="400">
+                <template v-slot:subtitle="{ subtitle }">
+                    <div v-html="subtitle"></div>
+                </template>
+            </v-list>
+        </v-menu>
+
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <span v-bind="props" style="cursor:pointer" class="mx-5 mr-10">
+                    <v-chip link>
+                        <v-badge dot bottom offset-y="10" color="green">
+                            <v-avatar size="30">
+                                <v-img src="../assets/redoy.jpg"></v-img>
+                            </v-avatar>
+                        </v-badge>
+                        <span>Redoy kumar das </span>
+                    </v-chip>
+                </span>
+            </template>
+            <v-list>
+                <v-list>
+                    <v-list-item :prepend-avatar="require('../assets/redoy.jpg')" title="Redoy kumar das"
+                        subtitle="redoykumar0@gmail.com">
+                        <template v-slot:append>
+                            <v-btn size="small" variant="text" icon="mdi-menu-down"></v-btn>
+                        </template>
+                    </v-list-item>
+                </v-list>
+
+                <v-divider></v-divider>
+                <v-list-item v-for="(item, i) in items1" :key="i" :value="item" active-color="primary">
+                    <template v-slot:prepend>
+                        <v-icon :icon="item.icon"></v-icon>
+                    </template>
+
+                    <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "top-bar", data: () => ({
+        items: [
+            { type: 'subheader', title: 'Today' },
+            {
+                prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                title: 'Brunch this weekend?',
+                subtitle: `<span class="text-primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+            },
+            { type: 'divider', inset: true },
+            {
+                prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                title: 'Summer BBQ',
+                subtitle: `<span class="text-primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+            },
+            { type: 'divider', inset: true },
+            {
+                prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                title: 'Oui oui',
+                subtitle: '<span class="text-primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+            },
+            { type: 'divider', inset: true },
+            {
+                prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                title: 'Birthday gift',
+                subtitle: '<span class="text-primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+            },
+            { type: 'divider', inset: true },
+            {
+                prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                title: 'Recipe to try',
+                subtitle: '<span class="text-primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+            },
+        ],
+        items1: [
+            { text: 'Profile', icon: 'mdi-account' },
+            { text: 'Change Password', icon: 'mdi-key' },
+            { text: 'Setting', icon: 'mdi-cog' },
+            { text: 'Logout', icon: 'mdi-logout' },]
+    }),
+}
+</script>
+
+<style>
+
+</style>
